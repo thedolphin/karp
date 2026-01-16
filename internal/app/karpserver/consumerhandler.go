87344390ed758420dac6b113f90640dbeb1357be
Alexander Rumyantsev <github.com@rumyantsev.com>
2026-01-16 12:07:42 +0300

    fix claims field logging so that logstash won't unwrap it

--- internal/app/karpserver/consumerhandler.go
+++ internal/app/karpserver/consumerhandler.go
@@ -2,6 +2,8 @@ package karpserver
 
 import (
 	"log/slog"
+	"strconv"
+	"strings"
 
 	"github.com/IBM/sarama"
 )
@@ -16,7 +18,7 @@ func (h *ConsumerGroupAggregatingHandler) Setup(session sarama.ConsumerGroupSess
 	h.session = session
 	slog.Info("Joined consumer group",
 		"request-id", h.requestId,
-		"claims", session.Claims(),
+		"claims", claimsToString(session.Claims()),
 		"member-id", session.MemberID(),
 		"generation-id", session.GenerationID(),
 	)
@@ -27,7 +29,7 @@ func (h *ConsumerGroupAggregatingHandler) Setup(session sarama.ConsumerGroupSess
 func (h *ConsumerGroupAggregatingHandler) Cleanup(session sarama.ConsumerGroupSession) error {
 	slog.Info("Leaved consumer group",
 		"request-id", h.requestId,
-		"claims", session.Claims(),
+		"claims", claimsToString(session.Claims()),
 		"member-id", session.MemberID(),
 		"generation-id", session.GenerationID(),
 	)
@@ -61,4 +63,33 @@ func NewConsumerGroupAggregatingHandler(messages chan<- *sarama.ConsumerMessage,
 		requestId: reqiestId,
 		messages:  messages,
 	}
+}
+
+func claimsToString(claims map[string][]int32) string {
+
+	var s strings.Builder
+
+	f := true
+	for topic, partitions := range claims {
+		if f {
+			f = false
+		} else {
+			s.WriteByte(' ')
+		}
+
+		s.WriteString(topic)
+		s.WriteByte('(')
+
+		for i, partition := range partitions {
+			if i > 0 {
+				s.WriteByte(',')
+			}
+
+			s.WriteString(strconv.FormatInt(int64(partition), 10))
+		}
+
+		s.WriteByte(')')
+	}
+
+	return s.String()
 }
