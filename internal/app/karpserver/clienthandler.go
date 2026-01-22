e774251533a45abf1e026888e345e7ada847c5e3
Alexander Rumyantsev <github.com@rumyantsev.com>
2026-01-22 21:45:34 +0300

    Serve() refactoring; lua moved to consumer for parallel processing

--- internal/app/karpserver/clienthandler.go
+++ internal/app/karpserver/clienthandler.go
@@ -11,29 +11,31 @@ import (
 	"github.com/IBM/sarama"
 	"github.com/thedolphin/karp/internal/pkg/karputils"
 	"github.com/thedolphin/karp/pkg/karp"
-	"github.com/thedolphin/luarunner"
 	"golang.org/x/sync/errgroup"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
-type ClientHandler struct {
-	requestId            uint64
-	clientId             string
-	cluster              string
-	user                 string
-	group                string
+type KarpClient struct {
+	requestId uint64
+	clientId  string
+	user      string
+	group     string
+	topics    []string
+	cluster   string
+}
+
+type KarpClientHandler struct {
+	client               *KarpClient
 	consumerGroupHandler *ConsumerGroupAggregatingHandler
 	consumer             sarama.ConsumerGroup
-	messages             chan *sarama.ConsumerMessage
 	stream               grpc.BidiStreamingServer[karp.Ack, karp.Message]
-	topics               []string
 	window               *FlowWindow
 	saramaCfg            *sarama.Config
 }
 
-func (kch *ClientHandler) clientStreamHandler() error {
+func (kch *KarpClientHandler) clientStreamHandler() error {
 
 	for {
 		ack, err := kch.stream.Recv()
@@ -46,8 +48,8 @@ func (kch *ClientHandler) clientStreamHandler() error {
 		}
 
 		slog.Debug("Received ack from client",
-			"request-id", kch.requestId,
-			"client-id", kch.clientId,
+			"request-id", kch.client.requestId,
+			"client-id", kch.client.clientId,
 			"message", karputils.MessageFmt(ack.ID, ack.Topic, ack.Partition, ack.Offset))
 
 		kch.window.Chase(ack.ID)
@@ -62,21 +64,7 @@ func (kch *ClientHandler) clientStreamHandler() error {
 	}
 }
 
-func (kch *ClientHandler) serverStreamHandler() error {
-
-	var (
-		lua *luarunner.LuaRunner
-		err error
-	)
-
-	if luaScript, ok := LuaScripts[kch.cluster]; ok {
-		lua, err = luaInit(luaScript)
-		if err != nil {
-			return fmt.Errorf("error initializing lua filter: %w", err)
-		}
-
-		defer lua.Close()
-	}
+func (kch *KarpClientHandler) serverStreamHandler() error {
 
 	timer := time.NewTimer(Config.Ping)
 	defer timer.Stop()
@@ -89,46 +77,22 @@ func (kch *ClientHandler) serverStreamHandler() error {
 		}
 
 		select {
-		case msg, ok := <-kch.messages:
+		case msg, ok := <-kch.consumerGroupHandler.Messages():
 			if !ok {
-				slog.Info("No more messages to send, sending Done to client", "request-id", kch.requestId)
+				slog.Info("No more messages to send, sending Done to client", "request-id", kch.client.requestId)
 				err = kch.stream.Send(&karp.Message{Done: true})
 				if err != nil {
 					slog.Error("Error sending Done to client",
-						"request-id", kch.requestId,
-						"client-id", kch.clientId,
+						"request-id", kch.client.requestId,
+						"client-id", kch.client.clientId,
 						"err", err)
 				}
 				return err
 			}
 
-			if lua != nil {
-
-				// luaProcess changes msg.Value if necessary
-				pass, err := luaProcess(lua, msg, kch.user, kch.group)
-				if err != nil {
-					slog.Error("Error filtering message: error executing script",
-						"request-id", kch.requestId,
-						"client-id", kch.clientId,
-						"message", karputils.MessageFmt(id, msg.Topic, msg.Partition, msg.Offset),
-						"err", err)
-
-					return fmt.Errorf("error filtering message: %w", err)
-				}
-
-				if !pass {
-					slog.Debug("Message filtered out",
-						"request-id", kch.requestId,
-						"client-id", kch.clientId,
-						"message", karputils.MessageFmt(id, msg.Topic, msg.Partition, msg.Offset))
-
-					continue
-				}
-			}
-
 			slog.Debug("Sending message to client",
-				"request-id", kch.requestId,
-				"client-id", kch.clientId,
+				"request-id", kch.client.requestId,
+				"client-id", kch.client.clientId,
 				"message", karputils.MessageFmt(id, msg.Topic, msg.Partition, msg.Offset))
 
 			headers := make([]*karp.MessageHeader, len(msg.Headers))
@@ -154,12 +118,12 @@ func (kch *ClientHandler) serverStreamHandler() error {
 				return fmt.Errorf("error sending message: %w", err)
 			}
 
-			promServedMessages.WithLabelValues(kch.clientId, kch.cluster, msg.Topic).Inc()
+			promServedMessages.WithLabelValues(kch.client.clientId, kch.client.cluster, msg.Topic).Inc()
 
 		case <-timer.C:
 			slog.Debug("Sending PING to client",
-				"request-id", kch.requestId,
-				"client-id", kch.clientId,
+				"request-id", kch.client.requestId,
+				"client-id", kch.client.clientId,
 				"message id", id)
 
 			err = kch.stream.Send(&karp.Message{
@@ -175,49 +139,47 @@ func (kch *ClientHandler) serverStreamHandler() error {
 	}
 }
 
-func (kch *ClientHandler) kafkaConsumer(ctx context.Context) error {
+func (kch *KarpClientHandler) kafkaConsumer(ctx context.Context) error {
 
-	defer close(kch.messages)
+	defer kch.consumerGroupHandler.Close()
 
 	for {
+		slog.Info("Starting consuming session", "request-id", kch.client.requestId)
 
-		slog.Info("Starting consuming session", "request-id", kch.requestId)
-
-		err := kch.consumer.Consume(ctx, kch.topics, kch.consumerGroupHandler)
+		err := kch.consumer.Consume(ctx, kch.client.topics, kch.consumerGroupHandler)
 		if err != nil {
-			slog.Error("Consuming session stopped with error", "request-id", kch.requestId, "err", err)
+			slog.Error("Consuming session stopped with error", "request-id", kch.client.requestId, "err", err)
 			return fmt.Errorf("consuming stopped, err: %w", err)
 		} else {
-			slog.Info("Consuming session stopped with no error", "request-id", kch.requestId)
+			slog.Info("Consuming session stopped with no error", "request-id", kch.client.requestId)
 		}
 
 		// If the context is canceled, it means there's an error somewhere.
 		// Otherwise, it's a rebalance and we need to restart Consume().
 		if err = ctx.Err(); err != nil {
-			slog.Info("Consuming loop stopped", "request-id", kch.requestId)
-			return io.EOF
+			slog.Info("Consuming loop stopped", "request-id", kch.client.requestId)
+			return err
 		}
 	}
 }
 
-func (kch *ClientHandler) Serve(ctx context.Context) error {
+func (kch *KarpClientHandler) Serve(ctx context.Context) error {
 
 	var err error
 	if kch.consumer, err = sarama.NewConsumerGroup(
-		Config.Clusters[kch.cluster].Brokers,
-		kch.group,
+		Config.Clusters[kch.client.cluster].Brokers,
+		kch.client.group,
 		kch.saramaCfg,
 	); err != nil {
-		slog.Error("Error creating consumer group", "request-id", kch.requestId, "err", err)
+		slog.Error("Error creating consumer group", "request-id", kch.client.requestId, "err", err)
 		return status.Errorf(codes.Internal, "KARP Server Internal Error: Error creating consumer group: %v", err)
 	}
 
-	kch.messages = make(chan *sarama.ConsumerMessage, 128)
-	kch.consumerGroupHandler = NewConsumerGroupAggregatingHandler(kch.messages, kch.requestId)
-
 	serveGrp, serveCtx := errgroup.WithContext(ctx)
-
 	kch.window = NewFlowWindow(serveCtx, Config.WindowSize, Config.Timeout)
+
+	luaScript, _ := LuaScripts[kch.client.cluster]
+	kch.consumerGroupHandler = NewConsumerGroupAggregatingHandler(kch.client, luaScript)
 
 	// client sends close -> kafka consumer stops -> server goroutine stops
 	// server goroutine stops -> kafka consumer stops -> clientStreamHandler HANGS waiting for client to close connection
@@ -251,7 +213,7 @@ func (kch *ClientHandler) Serve(ctx context.Context) error {
 		if errors.Is(err, sarama.ErrOutOfBrokers) {
 			err = sarama.ErrOutOfBrokers
 		}
-		slog.Error("Error while handling client", "request-id", kch.requestId, "err", err)
+		slog.Error("Error while handling client", "request-id", kch.client.requestId, "err", err)
 		return status.Errorf(codes.Internal, "KARP Server Internal Error: %v", err)
 	}
 
--- internal/app/karpserver/consumerhandler.go
+++ internal/app/karpserver/consumerhandler.go
@@ -1,38 +1,60 @@
 package karpserver
 
 import (
+	"fmt"
 	"log/slog"
-	"strconv"
-	"strings"
 
 	"github.com/IBM/sarama"
+	"github.com/thedolphin/karp/internal/pkg/karputils"
+	"github.com/thedolphin/luarunner"
 )
 
 type ConsumerGroupAggregatingHandler struct {
-	requestId uint64
 	session   sarama.ConsumerGroupSession
-	messages  chan<- *sarama.ConsumerMessage
+	luaScript string
+	luaVMs    map[string]map[int32]*luarunner.LuaRunner
+	client    *KarpClient
+	messages  chan *sarama.ConsumerMessage
 }
 
 func (h *ConsumerGroupAggregatingHandler) Setup(session sarama.ConsumerGroupSession) error {
 	h.session = session
+	claims := session.Claims()
 	slog.Info("Joined consumer group",
-		"request-id", h.requestId,
-		"claims", claimsToString(session.Claims()),
+		"request-id", h.client.requestId,
+		"claims", karputils.ClaimFmt(claims),
 		"member-id", session.MemberID(),
 		"generation-id", session.GenerationID(),
 	)
+	var err error
+
+	if h.luaScript != "" {
+		for topic, partitions := range claims {
+			for _, partition := range partitions {
+				h.luaVMs[topic][partition], err = luaInit(h.luaScript)
+				if err != nil {
+					return err
+				}
+			}
+		}
+	}
 
 	return nil
 }
 
 func (h *ConsumerGroupAggregatingHandler) Cleanup(session sarama.ConsumerGroupSession) error {
 	slog.Info("Leaved consumer group",
-		"request-id", h.requestId,
-		"claims", claimsToString(session.Claims()),
+		"request-id", h.client.clientId,
+		"claims", karputils.ClaimFmt(session.Claims()),
 		"member-id", session.MemberID(),
 		"generation-id", session.GenerationID(),
 	)
+
+	for _, partitions := range h.luaVMs {
+		for _, luaVM := range partitions {
+			luaVM.Close()
+		}
+	}
 
 	return nil
 }
@@ -40,13 +62,36 @@ func (h *ConsumerGroupAggregatingHandler) Cleanup(session sarama.ConsumerGroupSe
 func (h *ConsumerGroupAggregatingHandler) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
 
 	slog.Debug("Starting single partition consuming",
-		"request-id", h.requestId,
+		"request-id", h.client.clientId,
 		"topic", claim.Topic(),
 		"partition", claim.Partition(),
 	)
 
+	defer slog.Debug("Stopped single partition consuming",
+		"request-id", h.client.clientId,
+		"topic", claim.Topic(),
+		"partition", claim.Partition(),
+	)
+
+	var lua *luarunner.LuaRunner
+	if h.luaScript != "" {
+		lua = h.luaVMs[claim.Topic()][claim.Partition()]
+	}
+
 loop:
 	for msg := range claim.Messages() {
+
+		if lua != nil {
+			// luaProcess changes msg.Value if necessary
+			pass, err := luaProcess(lua, msg, h.client.user, h.client.group)
+			if err != nil {
+				return fmt.Errorf("error filtering message: %w", err)
+			}
+			if !pass {
+				continue
+			}
+		}
+
 		select {
 		case h.messages <- msg:
 		case <-session.Context().Done():
@@ -54,47 +99,21 @@ loop:
 		}
 	}
 
-	slog.Debug("Stopped single partition consuming",
-		"request-id", h.requestId,
-		"topic", claim.Topic(),
-		"partition", claim.Partition(),
-	)
-
 	return nil
 }
 
-func NewConsumerGroupAggregatingHandler(messages chan<- *sarama.ConsumerMessage, reqiestId uint64) *ConsumerGroupAggregatingHandler {
-	return &ConsumerGroupAggregatingHandler{
-		requestId: reqiestId,
-		messages:  messages,
-	}
+func (h *ConsumerGroupAggregatingHandler) Messages() <-chan *sarama.ConsumerMessage {
+	return h.messages
 }
 
-func claimsToString(claims map[string][]int32) string {
+func (h *ConsumerGroupAggregatingHandler) Close() {
+	close(h.messages)
+}
 
-	var s strings.Builder
-
-	f := true
-	for topic, partitions := range claims {
-		if f {
-			f = false
-		} else {
-			s.WriteByte(' ')
-		}
-
-		s.WriteString(topic)
-		s.WriteByte('(')
-
-		for i, partition := range partitions {
-			if i > 0 {
-				s.WriteByte(',')
-			}
-
-			s.WriteString(strconv.FormatInt(int64(partition), 10))
-		}
-
-		s.WriteByte(')')
+func NewConsumerGroupAggregatingHandler(client *KarpClient, luaScript string) *ConsumerGroupAggregatingHandler {
+	return &ConsumerGroupAggregatingHandler{
+		luaScript: luaScript,
+		client:    client,
+		messages:  make(chan *sarama.ConsumerMessage, 128),
 	}
-
-	return s.String()
 }
